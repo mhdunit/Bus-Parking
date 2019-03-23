6781f97c3a688a5a20bdb89768478a2fa513cf2a
MHDUNIT <mhdunit01@gmail.com>
2019-03-23 14:34:30 +0430

    Fixing garage bus selection button

---
+++ Assets/BusParking2/MHD/Scripts/FixNexPrevButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FixNexPrevButton : MonoBehaviour
+{
+    public CarSelect CS;
+    public GameObject Next, Prev;
+    public int EnableDisableButtonTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BusNextPrevCheck();
+    }
+    void BusNextPrevCheck()
+    {
+        if (CS.cars.Length - 1 == PlayerPrefs.GetInt("BusID"))
+            Next.SetActive(false);
+        else
+            Next.SetActive(true);
+
+        if (PlayerPrefs.GetInt("BusID") == 0)
+            Prev.SetActive(false);
+        else
+            Prev.SetActive(true);
+    }
+    public void SetTrueFalse()
+    {
+        BusNextPrevCheck();
+        StartCoroutine(SetTrueFalseAfter1Minute());  
+    }
+    IEnumerator SetTrueFalseAfter1Minute()
+    {
+
+        Next.GetComponent<Button>().enabled = false;
+        Prev.GetComponent<Button>().enabled = false;
+        yield return new WaitForSeconds(EnableDisableButtonTime);
+        Next.GetComponent<Button>().enabled = true;
+        Prev.GetComponent<Button>().enabled = true;
+
+    }
+ 
+}
