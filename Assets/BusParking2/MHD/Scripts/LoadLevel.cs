5861772cff05dbd1a369114e3ea9d1b6e9921b38
MHDUNIT <mhdunit01@gmail.com>
2019-04-04 21:15:58 +0430

    Fixing UI problem and adding start loading scene

---
+++ Assets/BusParking2/MHD/Scripts/LoadLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadLevel : MonoBehaviour
+{
+    public string SceneName;
+
+    void Start()
+    {
+        SceneManager.LoadSceneAsync(SceneName);
+    }
+    // Start is called before the first frame update
+
+}
