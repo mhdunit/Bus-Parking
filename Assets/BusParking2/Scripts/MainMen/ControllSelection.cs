7f28a21b605d445a276ce1645beb7b0e84cd43db
MHDUNIT <mhdunit01@gmail.com>
2019-04-04 14:34:10 +0430

    Garage setting completed

--- Assets/BusParking2/Scripts/MainMen/ControllSelection.cs
+++ Assets/BusParking2/Scripts/MainMen/ControllSelection.cs
@@ -1,17 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ControllSelection : MonoBehaviour {
 
-	public void SelectControl (int id) 
-	{
+    public GameObject[] GameobjectControlType;
+    public Sprite[] ControlTypeOn, ControlTypeOff;
 
-		PlayerPrefs.SetInt ("Controll", id);
-	}
-	public void SetFalse(GameObject target)
+    void OnEnable()
+    {
+        switch (PlayerPrefs.GetInt("Controll"))
+        {
+            case 0:
+                GameobjectControlType[0].GetComponent<Image>().sprite = ControlTypeOn[0];
+                GameobjectControlType[1].GetComponent<Image>().sprite = ControlTypeOff[1];
+                GameobjectControlType[2].GetComponent<Image>().sprite = ControlTypeOff[2];
+                break;
+            case 1:
+                GameobjectControlType[0].GetComponent<Image>().sprite = ControlTypeOff[0];
+                GameobjectControlType[1].GetComponent<Image>().sprite = ControlTypeOn[1];
+                GameobjectControlType[2].GetComponent<Image>().sprite = ControlTypeOff[2];
+                break;
+            case 2:
+                GameobjectControlType[0].GetComponent<Image>().sprite = ControlTypeOff[0];
+                GameobjectControlType[1].GetComponent<Image>().sprite = ControlTypeOff[1];
+                GameobjectControlType[2].GetComponent<Image>().sprite = ControlTypeOn[2];
+                break;
+        }
+    }
+
+    public void SelectControl (int id) 
 	{
-		target.SetActive (false);
+		PlayerPrefs.SetInt ("Controll", id);
+        GameobjectControlType[id].GetComponent<Image>().sprite = ControlTypeOn[id];
+        switch (id)
+        {
+            case 0:
+                GameobjectControlType[1].GetComponent<Image>().sprite = ControlTypeOff[1];
+                GameobjectControlType[2].GetComponent<Image>().sprite = ControlTypeOff[2];
+                break;
+            case 1:
+                GameobjectControlType[0].GetComponent<Image>().sprite = ControlTypeOff[0];
+                GameobjectControlType[2].GetComponent<Image>().sprite = ControlTypeOff[2];
+                break;
+            case 2:
+                GameobjectControlType[0].GetComponent<Image>().sprite = ControlTypeOff[0];
+                GameobjectControlType[1].GetComponent<Image>().sprite = ControlTypeOff[1];
+                break;
+        }
+            
+       
 	}
+
 
 
 }
