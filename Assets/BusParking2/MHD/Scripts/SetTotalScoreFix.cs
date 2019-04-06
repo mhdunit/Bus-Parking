5029c4a9635f0069cca5d84280cc56c2b6c49634
MHDUNIT <mhdunit01@gmail.com>
2019-04-06 19:15:52 +0430

    Fixing some bug while set reward

---
+++ Assets/BusParking2/MHD/Scripts/SetTotalScoreFix.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetTotalScoreFix : MonoBehaviour
+{
+    public Text totalMenuFix;
+
+     void OnEnable()
+    {
+        if (gameObject.activeSelf)
+        {
+            totalMenuFix.text = PlayerPrefs.GetInt("Coins").ToString();
+        }
+    }
+
+}
