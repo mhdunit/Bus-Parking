98e765c9c1eed16d8b96101948e1d358a163988c
MHDUNIT <mhdunit01@gmail.com>
2019-04-07 23:19:07 +0430

    Fixing Rewarded Video Text in Success Menu

--- Assets/BusParking2/MHD/Scripts/RewardedSound.cs
+++ Assets/BusParking2/MHD/Scripts/RewardedSound.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class RewardedSound : MonoBehaviour
@@ -12,17 +13,24 @@ public class RewardedSound : MonoBehaviour
     public GameObject[] Stars;
     int AwardedCoins;
 
+    void OnEnable()
+    {
+        {
+            if (Application.loadedLevelName == "Day_Bus")
+            {
+                for (int i = 0; i < Stars.Length; i++)
+                {
+                    if (Stars[i].activeSelf)
+                    {
+                        AwardedCoins = i + 1;
+                    }
+                }
+            }
+        }
+    }
 
     public void RewardedAudioAndScore()
     {
-        for (int i = 0; i < Stars.Length; i++)
-        {
-            if (Stars[i].activeSelf) 
-            {
-                AwardedCoins = i + 1;
-            }
-        }
-
         AS.PlayOneShot(AC);
 
         if (Application.loadedLevelName == "Garage_Bus")
