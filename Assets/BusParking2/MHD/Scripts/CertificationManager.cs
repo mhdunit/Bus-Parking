bac3b13b98a055d7fb3c5aae10664e31a68ed63b
MHDUNIT <mhdunit01@gmail.com>
2019-05-02 23:45:26 +0430

    Bus Driver Certification Added(UI,Logic)

---
+++ Assets/BusParking2/MHD/Scripts/CertificationManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CertificationManager : MonoBehaviour
+{
+    public GameObject CertificationPanel, CertStamp,Success;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("BusLevelID") == 0)
+            CertificationPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
