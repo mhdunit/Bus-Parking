0f6070450f797fd3bc0abb305e820f7d37a2fee5
MHDUNIT <mhdunit01@gmail.com>
2019-05-03 10:35:42 +0430

    Working on Language

---
+++ Assets/BusParking2/MHD/Scripts/EntranceLanguage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceLanguage : MonoBehaviour
+{
+    public enum Language
+    {
+        English,
+        Farsi
+    }
+
+    public Language DefaultLanguage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("Language"))
+        {
+            if (DefaultLanguage == Language.English)
+            {
+                PlayerPrefs.SetString("Language", "English");
+            }
+            else if (DefaultLanguage == Language.Farsi)
+            {
+                PlayerPrefs.SetString("Language", "Farsi");
+            }
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/BusParking2/MHD/Scripts/Language.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Language : MonoBehaviour
+{
+    public Text English, Farsi;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetString("Language") == "English")
+        {
+            English.color = Color.green;
+            Farsi.color = Color.white;
+        }
+        else if (PlayerPrefs.GetString("Language") == "Farsi")
+        {
+            English.color = Color.white;
+            Farsi.color = Color.green;
+        }
+    }
+    public void EnglishSelected()
+    {
+        English.color = Color.green;
+        Farsi.color = Color.white;
+        PlayerPrefs.SetString("Language","English");
+    }
+    public void FarsiSelected()
+    {
+        English.color = Color.white;
+        Farsi.color = Color.green;
+        PlayerPrefs.SetString("Language", "Farsi");
+    }
+}
